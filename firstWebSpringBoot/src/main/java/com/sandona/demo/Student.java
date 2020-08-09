fba48429865dba72311e7504dccd785fc109bbba
sandos1 <sandona.soro@yahoo.fr>
2020-08-09 00:37:50 -0500

    change in proertie.application and Wellcome class

---
+++ firstWebSpringBoot/src/main/java/com/sandona/demo/Student.java
@@ -0,0 +1,49 @@
+package com.sandona.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	
+	private String name;
+	private int age;
+	private String school;
+	public Student() {
+		
+	}
+	
+	public Student(String name, int age, String school) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.school = school;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	
+	
+
+}
--- firstWebSpringBoot/src/main/java/com/sandona/demo/Wellcome.java
+++ firstWebSpringBoot/src/main/java/com/sandona/demo/Wellcome.java
@@ -1,14 +1,27 @@
 package com.sandona.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Wellcome {
+	@Autowired
+	public Student st;
 	
-	@RequestMapping("/welcome")
-	public String showwellcom() {
-		return "hi Soro";
+	
+	
+	//@RequestMapping("/welcome")
+	@GetMapping("/welcome")
+	public List<Student> showwellcom() {
+		return  Arrays.asList(
+				new Student("soro",22,"MSUM"),
+				new Student("sandona",22,"NDSU")
+		);
 	}
 
 }
